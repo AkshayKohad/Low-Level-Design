65341aa29659778a8b96ba48a39b6a1934740f0b
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2026-06-22 21:56:26 +0530

    Create Decorator pattern.java

---
+++ Design Patterns/Structural Design Patterns/Decorator pattern.java
@@ -0,0 +1,86 @@
+interface Icecream{
+    String getDescription();
+    double getCost();
+}
+
+class Vanilla implements Icecream{
+    
+    @Override
+    public String getDescription(){
+        return "Vanilla Cone";
+    }
+    
+    @Override
+    public double getCost(){
+        return 2.0;
+    }
+}
+
+abstract class IceCreamDecorator implements Icecream{
+    protected Icecream icecream;
+    
+    IceCreamDecorator(Icecream icecream){
+        this.icecream = icecream;
+    }
+    
+    @Override
+    public String getDescription(){
+        return icecream.getDescription();
+    }
+    
+    @Override
+    public double getCost(){
+        return icecream.getCost();
+    }
+}
+
+
+class ChocolateTopping extends IceCreamDecorator{
+    
+    ChocolateTopping(Icecream icecream){
+        super(icecream);
+    }
+    
+    @Override
+    public String getDescription(){
+        return icecream.getDescription() + " ChocolateToppings";
+    }
+    
+    @Override
+    public double getCost(){
+        return icecream.getCost() + 1.0;
+    }
+}
+
+// Concrete decorator: Nuts topping
+class NutsTopping extends IceCreamDecorator {
+
+    public NutsTopping(Icecream icecream) {
+        super(icecream);
+    }
+
+    @Override
+    public String getDescription() {
+        return icecream.getDescription() + ", Nuts";
+    }
+
+    @Override
+    public double getCost() {
+        return icecream.getCost() + 1.00;
+    }
+}
+
+
+class Main {
+  public static void main(String[] args) {
+    Icecream iceCream = new Vanilla();
+    System.out.println(iceCream.getDescription());
+    System.out.println("Cost: $" + iceCream.getCost());
+    iceCream = new ChocolateTopping(iceCream);
+    iceCream = new NutsTopping(iceCream);
+    System.out.println(iceCream.getDescription());
+    System.out.println("Cost: $" + iceCream.getCost());
+  }
+}
+
+
